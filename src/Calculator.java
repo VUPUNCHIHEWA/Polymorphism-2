be480cfd6539ac77f506e80c1fb3e2a2fbac6cb2
ASUS <40-bis-0023@kdu.ac.lk>
2023-10-04 17:04:52 +0530

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+class Calculator {
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+    // Method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    // Method to add two doubles
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
